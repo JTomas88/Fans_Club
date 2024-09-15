0c0d67c9e75f8f578bf2eb67fa7804977ea97bcf
JTomas88 <juan.tomas88@hotmail.com>
2024-09-15 22:17:47 +0200

    navbar

---
+++ src/front/js/component/Jumbotron/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import React, { useContext, useEffect } from "react";
+import styles from "./jumbotron.module.css";
+
+
+export const Jumbotron = () => {
+    return (
+        <div className={styles.container}>
+            <p className={styles.title}>Club Fans Oficial Sienna</p>
+        </div>
+    )
+}
